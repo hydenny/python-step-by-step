5058b4947de8cf6f128fcad635814ac15b852304
hayoung0402 <41022183+hydenny@users.noreply.github.com>
2021-01-11 23:49:48 +0900

    Add files via upload

---
+++ for문/2번 A+B_3.py
@@ -0,0 +1,9 @@
+N = int(input())
+for N in range(0, N):
+    A, B = input().split()
+    A = int(A)
+    B = int(B)
+    print(A+B)
+    '''
+    range의 경우 0~N까지, 즉 0에서부터 실행하고 N이 되면 종료하는 방식으로 생각한다.
+    '''
