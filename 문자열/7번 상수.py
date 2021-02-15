9669e761c12b8aa6e9df683d6bafa699516e3138
hayoung0402 <41022183+hydenny@users.noreply.github.com>
2021-02-15 22:54:21 +0900

    Add files via upload

---
+++ 문자열/7번 상수.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+A, B = input().split()
+
+a = int(A[2]+A[1]+A[0])
+b = int(B[2]+B[1]+B[0])
+
+if a > b:
+    print(a)
+else:
+    print(b)
+
+
+# In[ ]:
+
+
+
+
