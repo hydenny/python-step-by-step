f4eedea0469f27675d83476ae7a01af9bda230a3
hayoung0402 <41022183+hydenny@users.noreply.github.com>
2021-02-17 23:40:13 +0900

    Add files via upload

---
+++ 문자열/9번 크로아티아 알파벳.py
@@ -0,0 +1,29 @@
+string = input()
+
+count = len(string)
+
+if "c=" in string:
+    count -= string.count("c=")
+
+if "c-" in string:
+    count -= string.count("c-")
+
+if "dz=" in string:
+    count -= string.count("dz=")
+
+if "d-" in string:
+    count -= string.count("d-")
+
+if "lj" in string:
+    count -= string.count("lj")
+
+if "nj" in string:
+    count -= string.count("nj")
+
+if "s=" in string:
+    count -= string.count("s=")
+
+if "z=" in string:
+    count -= string.count("z=")
+
+print(count)
