fcc0f1237626302a7ca6c6902556e4bd9a738173
hayoung0402 <41022183+hydenny@users.noreply.github.com>
2021-02-17 23:39:53 +0900

    Update 9번 크로아티아 알파벳.py

--- 문자열/9번 크로아티아 알파벳.py
+++ 문자열/9번 크로아티아 알파벳.py
@@ -1,39 +1,29 @@
 string = input()
 
-for i in string:
+count = len(string)
 
-    if i == "c=":
-        i = string.replace(i, "A")
-        print("check1")
-    if i == "c-":
-        i = string.replace(i, "B")
-        print("check2")
+if "c=" in string:
+    count -= string.count("c=")
+   
+if "c-" in string:
+    count -= string.count("c-")
 
-    if i == "dz=":
-        i = string.replace(i, "C")
-        print("check3")
+if "dz=" in string:
+    count -= string.count("dz=")
 
-    if i == "d-":
-        i = string.replace(i, "D")
-        print("check4")
+if "d-" in string:
+    count -= string.count("d-")
 
-    if i == "lj":
-        i = string.replace(i, "E")
-        print("check5")
+if "lj" in string:
+    count -= string.count("lj")
 
-    if i == "nj":
-        i = string.replace(i, "F")
-        print("check6")
+if "nj" in string:
+    count -= string.count("nj")
 
-    if i == "s=":
-        i = string.replace(i, "G")
-        print("check7")
+if "s=" in string:
+    count -= string.count("s=")
 
-    if i == "z=":
-        i = string.replace(i, "H")
-        print("check8")
+if "z=" in string:
+    count -= string.count("z=")
 
-
-print(string)
-
-print(len(string))
+print(count)
